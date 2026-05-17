7d1c8e30704c676db29fc9b93d7df827025a9d17
Moza36 <mozacoelhobonifacio@gmail.com>
2026-05-17 23:10:29 +0100

    Transformei o arquivo login.html para login.php

--- config.php
+++ config.php
@@ -1,10 +1,16 @@
 <?php
-// config.php
-// Conexão com o banco MySQL (XAMPP)
 
-$pdo = new PDO(
-  "mysql:host=localhost;dbname=biblioteca;charset=utf8mb4",
-  "root",
-  "",
-  [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
+try {
+    $pdo = new PDO(
+        "mysql:host=localhost;port=3307;dbname=biblioteca;charset=utf8mb4",
+        "root",
+        "",
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+
+    echo "Conexão realizada com sucesso!";
+
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
+?>
