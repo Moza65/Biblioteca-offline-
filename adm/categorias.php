319125dba39467d5490e5e8722a87ad71c63d36a
Groew-script <mistrbug@gmail.com>
2026-05-25 21:09:10 +0100

    Finalizado

--- adm/categorias.php
+++ adm/categorias.php
@@ -53,7 +53,7 @@ $categorias = $gerenciador->listarTodos();
         <header class="topbar">
             <div class="welcome-text">
                 <div class="page-title-row">
-                    <img src="../asset/icones/categorias.svg" alt="" class="page-title-icon">
+                    <img src="../asset/icones/folder.svg" alt="" class="page-title-icon">
                     <h1>Categorias</h1>
                 </div>
                 <p>Gerencie as categorias do acervo</p>
